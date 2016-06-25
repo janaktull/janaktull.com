78dcfb3188699c19063c890345bc5c0c0837e638
Chet Corcos <ccorcos@gmail.com>
2016-06-25 11:54:26 -0700

    new images

---
+++ reset.sh
@@ -0,0 +1,4 @@
+cd ~/Desktop/janaktull.github.io/
+git add .
+git reset --hard
+git pull origin master
---
+++ update.sh
@@ -0,0 +1,4 @@
+cd ~/Desktop/janaktull.github.io/
+git add .
+git commit -m "janak update"
+git push origin master
